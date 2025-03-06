acc1ab4d9f0f9f22ae3fb321b3f2fcabdb790dbc
rahilsheikh7 <rahilsheikh8267@gmail.com>
2025-03-06 15:55:06 +0530

    Update app.py (Render)

--- backend/app.py
+++ backend/app.py
@@ -221,4 +221,5 @@ def download():
         return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=port)
